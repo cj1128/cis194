b438b4e9e1a7946dd52f2fb49d5e3eed49889a00
CJ Ting <fatelovely1128@gmail.com>
2016-12-04 11:27:24 +0800

    Fix LogAnalysis bug

--- 02-ADTs/LogAnalysis.hs
+++ 02-ADTs/LogAnalysis.hs
@@ -1,23 +1,29 @@
 {-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
 import Log
+import Text.Read
+
+isInteger :: String -> Bool
+isInteger str = case readMaybe str :: Maybe Integer of
+                  Nothing -> False
+                  Just _ -> True
 
 parseMessage :: String -> LogMessage
 parseMessage s = case (words s) of
-  ("I":time:content) -> LogMessage Info (read time) (unwords content)
-  ("W":time:content) -> LogMessage Warning (read time) (unwords content)
-  ("E":level:time:content) -> LogMessage (Error (read level)) (read time) (unwords content)
-  otherwise -> Unknown s
+  ("I":time:content) | isInteger time  -> LogMessage Info (read time) (unwords content)
+  ("W":time:content) | isInteger time -> LogMessage Warning (read time) (unwords content)
+  ("E":level:time:content) | isInteger level && isInteger time -> LogMessage (Error (read level)) (read time) (unwords content)
+  _ -> Unknown s
 
 parse :: String -> [LogMessage]
 parse = map parseMessage . lines
 
 insert :: LogMessage -> MessageTree -> MessageTree
 insert (Unknown _) tree = tree
-insert log Leaf = Node Leaf log Leaf
-insert log@(LogMessage _ time1 _) (Node left root@(LogMessage _ time2 _) right)
-  | time1 < time2 = Node (insert log left) root right
-  | otherwise = Node left root (insert log right)
+insert msg Leaf = Node Leaf msg Leaf
+insert msg@(LogMessage _ time1 _) (Node left root@(LogMessage _ time2 _) right)
+  | time1 < time2 = Node (insert msg left) root right
+  | otherwise = Node left root (insert msg right)
 
 build :: [LogMessage] -> MessageTree
 build = foldr insert Leaf
@@ -30,10 +36,8 @@ whatWentWrong :: [LogMessage] -> [String]
 whatWentWrong = map getContent . inOrder . build . filter errorFilter 
 
 errorFilter :: LogMessage -> Bool
-errorFilter (LogMessage (Error level) _ _)
-  | level >= 50 = True
-  | otherwise = False
-errorFilter otherwise = False
+errorFilter (LogMessage (Error level) _ _) = level >= 50
+errorFilter _ = False
 
 getContent :: LogMessage -> String
 getContent (LogMessage _ _ content) = content
